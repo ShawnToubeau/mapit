9e0815cc9d4b3a5fa13b045bf0c55b044177c0fa
Shawn Toubeau <shawntoubeau@gmail.com>
2023-02-24 18:09:01 -0500

    fix: Changed nextjs runtime to edge-experimental

--- client/next.config.js
+++ client/next.config.js
@@ -1,23 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  /**
-   * this fixes an issue where the generated connect-web code imports protobuffer definitions
-   * with a .js extension even though they are .ts files.
-   * more context can be found here: https://github.com/bufbuild/connect-web/issues/236
-   * the fix is taken from here: https://github.com/bufbuild/connect-web-integration/blob/main/nextjs/next.config.js#L5L14
-   */
-  webpack: (config) => {
-    config.resolve = {
-      ...config.resolve,
-      extensionAlias: {
-        '.js': ['.ts', '.js'],
-      },
-    };
+	reactStrictMode: true,
+	swcMinify: true,
+	experimental: {
+		runtime: "experimental-edge",
+	},
+	/**
+	 * this fixes an issue where the generated connect-web code imports protobuffer definitions
+	 * with a .js extension even though they are .ts files.
+	 * more context can be found here: https://github.com/bufbuild/connect-web/issues/236
+	 * the fix is taken from here: https://github.com/bufbuild/connect-web-integration/blob/main/nextjs/next.config.js#L5L14
+	 */
+	webpack: (config) => {
+		config.resolve = {
+			...config.resolve,
+			extensionAlias: {
+				".js": [".ts", ".js"],
+			},
+		};
 
-    return config
-  },
-}
+		return config;
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
