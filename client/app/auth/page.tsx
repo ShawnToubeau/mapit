9f571e6ba1002ee3089aefce028ba2bfe2f1b220
Shawn Toubeau <shawntoubeau@gmail.com>
2023-02-26 17:20:42 -0500

    fix: Puts auth form into client component

--- client/app/auth/page.tsx
+++ client/app/auth/page.tsx
@@ -1,13 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
-
-const emailPlaceholderText = "Enter your email address";
+import AuthForm from "./AuthForm";
 
 export default function Page() {
-	const supabase = createBrowserSupabaseClient();
-
 	return (
 		<div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 			<div className="w-full max-w-md space-y-8">
@@ -19,32 +14,7 @@ export default function Page() {
 						</span>
 					</Link>
 				</div>
-				<Auth
-					showLinks
-					magicLink
-					redirectTo="http://localhost:3000/maps"
-					socialLayout="horizontal"
-					supabaseClient={supabase}
-					appearance={{ theme: ThemeSupa }}
-					localization={{
-						variables: {
-							magic_link: {
-								email_input_placeholder: emailPlaceholderText,
-							},
-							forgotten_password: {
-								email_input_placeholder: emailPlaceholderText,
-							},
-							sign_up: {
-								email_input_placeholder: emailPlaceholderText,
-								password_input_placeholder: "Enter a password",
-							},
-							sign_in: {
-								email_input_placeholder: emailPlaceholderText,
-								password_input_placeholder: "Enter your password",
-							},
-						},
-					}}
-				/>
+				<AuthForm />
 			</div>
 		</div>
 	);
