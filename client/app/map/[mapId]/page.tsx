7165dc327abdef09b7354319e2b4c5b5b37a3196
Shawn Toubeau <shawntoubeau@gmail.com>
2023-03-02 15:10:20 -0500

    feat: Adds map name to header

--- client/app/map/[mapId]/page.tsx
+++ client/app/map/[mapId]/page.tsx
@@ -1,8 +1,21 @@
 import ResponsiveEventMap from "../../../components/ResponsiveEventMap";
 import { FooterHeight, HeaderHeight } from "../../../constants";
 import Header from "../../../components/Header";
+import { Client } from "../../../hooks/use-client";
+import { EventMapService } from "../../../gen/proto/event_map_api/v1/event_map_api_connectweb";
+import { AnonAuthHeader } from "../../../utils/generate-auth-header";
 
-export default function Page({ params }: { params: { mapId: string } }) {
+export default async function Page({ params }: { params: { mapId: string } }) {
+	const client = Client(EventMapService);
+	const map = await client.getEventMap(
+		{
+			id: params.mapId,
+		},
+		{
+			headers: AnonAuthHeader(),
+		},
+	);
+
 	return (
 		<div
 			className="grid"
@@ -11,7 +24,7 @@ export default function Page({ params }: { params: { mapId: string } }) {
 				gridTemplateRows: `${HeaderHeight}px auto ${FooterHeight}px`,
 			}}
 		>
-			<Header />
+			<Header mapName={map.name} />
 			<ResponsiveEventMap mapId={params.mapId} />
 		</div>
 	);
