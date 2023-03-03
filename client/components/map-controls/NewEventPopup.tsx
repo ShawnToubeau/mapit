54b301a9fb0710d44d918611ca920ab3b779a88b
Shawn Toubeau <shawntoubeau@gmail.com>
2023-03-03 15:21:42 -0500

    feat: Center new event popups on the map

--- client/components/map-controls/NewEventPopup.tsx
+++ client/components/map-controls/NewEventPopup.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { LatLng } from "leaflet";
-import { Popup, useMapEvents } from "react-leaflet";
+import { useRef, useState } from "react";
+import { LatLng, Popup as LeafletPopup } from "leaflet";
+import { Popup, useMap, useMapEvents } from "react-leaflet";
 import EventForm from "../EventForm";
 import { useSupabase } from "../../context/supabase-provider";
 
@@ -9,11 +9,27 @@ interface NewEventPopupProps {
 }
 
 export default function NewEventPopup(props: NewEventPopupProps) {
+	const map = useMap();
 	const { session } = useSupabase();
 	const [position, setPosition] = useState<LatLng | null>(null);
+	const popupRef = useRef<LeafletPopup | null>(null);
+
 	useMapEvents({
-		click(e) {
-			setPosition(e.latlng);
+		click(event) {
+			setPosition(event.latlng);
+
+			// wait 20 ms for the popup container to populate in the DOM
+			setTimeout(() => {
+				const popupHeight = popupRef.current?.getElement()?.clientHeight;
+				if (popupHeight) {
+					// convert our lat/lng to pixel values
+					const px = map.project(event.latlng);
+					// translate the y-value by half of the popup's height + the marker height
+					px.y -= popupHeight / 2;
+					// convert back to a lat/lng and fly there, centering the popup in view
+					map.flyTo(map.unproject(px));
+				}
+			}, 20);
 		},
 	});
 
@@ -22,7 +38,7 @@ export default function NewEventPopup(props: NewEventPopupProps) {
 	}
 
 	return (
-		<Popup position={position}>
+		<Popup ref={popupRef} position={position}>
 			<EventForm
 				mapId={props.mapId}
 				session={session}
